ce86ea5a4777687265b6e3029a3fc10f13516ea9
Scala Steward <43047562+scala-steward@users.noreply.github.com>
2020-11-22 19:01:38 +0100

    Update http4s-blaze-client, ... to 0.21.11 (#79)

--- project/Deps.scala
+++ project/Deps.scala
@@ -1,7 +1,7 @@
 import sbt._
 
 object Deps {
-  lazy val http4sVersion    = "0.21.9"
+  lazy val http4sVersion    = "0.21.11"
   lazy val log4catsVersion  = "1.1.1"
   lazy val scalatestVersion = "3.2.3"
   lazy val circeVersion     = "0.13.0"

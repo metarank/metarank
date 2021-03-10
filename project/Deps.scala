0dac89d79c95336e0d092cfdeb7c59348d839730
Scala Steward <43047562+scala-steward@users.noreply.github.com>
2021-03-10 16:41:16 +0100

    Update lucene-analyzers-icu, lucene-core to 8.8.1 (#114)

--- project/Deps.scala
+++ project/Deps.scala
@@ -7,7 +7,7 @@ object Deps {
   lazy val circeVersion     = "0.13.0"
   lazy val circeYamlVersion = "0.13.1"
   lazy val fs2Version       = "2.5.0"
-  lazy val luceneVersion    = "8.8.0"
+  lazy val luceneVersion    = "8.8.1"
 
   val httpsDeps = Seq(
     "org.http4s" %% "http4s-dsl"          % http4sVersion,

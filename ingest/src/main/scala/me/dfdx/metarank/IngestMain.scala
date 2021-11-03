c2ea96566745d58e37331d60262d7dda02894ffe
Roman Grebennikov <grv@dfdx.me>
2021-11-03 18:54:48 +0000

    Dependency updates (#215)
    * bump all deps in build.sbt
    
    * compile fixes

--- ingest/src/main/scala/me/dfdx/metarank/IngestMain.scala
+++ ingest/src/main/scala/me/dfdx/metarank/IngestMain.scala
@@ -1,14 +1,13 @@
 package me.dfdx.metarank
 
 import java.util.concurrent.Executors
-
-import cats.effect.IO
 import me.dfdx.metarank.services.{HealthcheckService, IngestService}
-import cats.implicits._
-import me.dfdx.metarank.config.Config
-import org.http4s.HttpRoutes
 
 import scala.concurrent.ExecutionContext
+import cats.effect._
+import org.http4s._
+import cats.implicits._
+import me.dfdx.metarank.config.Config
 
 object IngestMain extends RestIOApp {
   override val executor            = ExecutionContext.fromExecutorService(Executors.newWorkStealingPool(4))

8e14cbd48aaf77b96bb329d857c933a0c59cb202
Grebennikov Roman <grv@dfdx.me>
2021-12-19 16:32:26 +0100

    restore also join state

--- src/main/scala/ai/metarank/flow/EventStateJoin.scala
+++ src/main/scala/ai/metarank/flow/EventStateJoin.scala
@@ -8,7 +8,8 @@ import io.findify.featury.model.{FeatureValue, Key}
 object EventStateJoin extends Join[EventState] {
   override def by(left: EventState): Key.Tenant = Tenant(left.event.tenant)
 
-  override def tags(left: EventState): List[Key.Tag] = ClickthroughJoin.scopes.flatMap(_.tags(left.event))
+  override def tags(left: EventState): List[Key.Tag] =
+    ClickthroughJoin.scopes.flatMap(_.tags(left.event))
 
   override def join(left: EventState, values: List[FeatureValue]): EventState =
     left.copy(state = left.state ++ values.map(fv => fv.key -> fv).toMap)

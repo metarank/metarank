4f69941c507219ed150fcac45037cf94d67932be
Roman Grebennikov <grv@dfdx.me>
2022-01-22 00:24:21 +0100

    fix race in feedback file sink-source (#268)

--- src/test/scala/ai/metarank/ingest/LocalDirSourceTest.scala
+++ src/test/scala/ai/metarank/ingest/LocalDirSourceTest.scala
@@ -5,22 +5,36 @@ import ai.metarank.source.LocalDirSource
 import ai.metarank.source.LocalDirSource.LocalDirWriter
 import ai.metarank.util.{FlinkTest, TestMetadataEvent}
 import better.files.File
+import cats.effect.Ref
+import cats.effect.unsafe.implicits.global
 import org.apache.flink.api.common.RuntimeExecutionMode
 import org.scalatest.flatspec.AnyFlatSpec
 import org.scalatest.matchers.should.Matchers
 import org.apache.flink.api.scala._
 
+import scala.util.Random
+
 class LocalDirSourceTest extends AnyFlatSpec with Matchers with FlinkTest {
   it should "accept events" in {
     env.setRuntimeMode(RuntimeExecutionMode.STREAMING)
     val path      = File.newTemporaryDirectory("events_").deleteOnExit()
-    val writer    = new LocalDirWriter(path)
+    val writer    = new LocalDirWriter(path, Ref.unsafe(0))
     val e1: Event = TestMetadataEvent("p1")
     val e2: Event = TestMetadataEvent("p2")
-    writer.write(e1)
-    writer.write(e2)
+    writer.write(e1).unsafeRunSync()
+    writer.write(e2).unsafeRunSync()
     val source = env.addSource(LocalDirSource(path.toString(), 1))
-    val result = source.executeAndCollect(100)
+    val result = source.executeAndCollect(1000)
     result shouldBe List(e1)
+  }
+
+  it should "not race" in {
+    val events = List.fill(1000)(TestMetadataEvent(Random.nextInt().toString))
+    val path   = File.newTemporaryDirectory("events_").deleteOnExit()
+    val writer = new LocalDirWriter(path, Ref.unsafe(0))
+    events.par.foreach(e => writer.write(e).unsafeRunSync())
+    val source = env.addSource(LocalDirSource(path.toString(), 1000))
+    val result = source.executeAndCollect(1000)
+    result.size shouldBe events.size
   }
 }

468a57f11cbb14c2accb804e3bead082272892ef
Scala Steward <43047562+scala-steward@users.noreply.github.com>
2022-02-13 19:59:39 +0100

    Update scalatest to 3.2.11 (#273)

--- project/Deps.scala
+++ project/Deps.scala
@@ -3,7 +3,7 @@ import sbt._
 object Deps {
   lazy val http4sVersion    = "1.0.0-M30"
   lazy val log4catsVersion  = "2.2.0"
-  lazy val scalatestVersion = "3.2.10"
+  lazy val scalatestVersion = "3.2.11"
   lazy val circeVersion     = "0.14.1"
   lazy val circeYamlVersion = "0.14.1"
   lazy val fs2Version       = "3.2.2"

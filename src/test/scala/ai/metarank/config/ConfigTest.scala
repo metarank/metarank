ce9a20d33428c746edc9b919a96b7e586ec8b2b5
Roman Grebennikov <grv@dfdx.me>
2022-03-19 01:31:16 +0000

    do not allow empty/dupe features (#316)

---
+++ src/test/scala/ai/metarank/config/ConfigTest.scala
@@ -0,0 +1,26 @@
+package ai.metarank.config
+
+import ai.metarank.feature.StringFeature.StringFeatureSchema
+import ai.metarank.model.Event.MetadataEvent
+import ai.metarank.model.FeatureScope.ItemScope
+import ai.metarank.model.FieldName
+import ai.metarank.model.FieldName.Metadata
+import cats.data.NonEmptyList
+import cats.effect.unsafe.implicits.global
+import io.findify.featury.model.FeatureConfig.ScalarConfig
+import io.findify.featury.model.Key.FeatureName
+import org.scalatest.flatspec.AnyFlatSpec
+import org.scalatest.matchers.should.Matchers
+
+import scala.util.Try
+
+class ConfigTest extends AnyFlatSpec with Matchers {
+  it should "fail on empty features" in {
+    Try(Config.validateConfig(Config(Nil, Nil)).unsafeRunSync()).isFailure shouldBe true
+  }
+
+  it should "fail on duplicates" in {
+    val dupe = StringFeatureSchema("foo", FieldName(Metadata, "foo"), ItemScope, NonEmptyList.of("x"))
+    Try(Config.validateConfig(new Config(List(dupe, dupe), Nil)).unsafeRunSync()).isFailure shouldBe true
+  }
+}

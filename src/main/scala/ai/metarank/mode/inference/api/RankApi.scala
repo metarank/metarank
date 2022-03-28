6360d6b4a8887e08b433f750eff9595031e6b7d5
Grebennikov Roman <grv@dfdx.me>
2022-03-28 14:47:42 +0200

    split heavy state requests to batches + tests

--- src/main/scala/ai/metarank/mode/inference/api/RankApi.scala
+++ src/main/scala/ai/metarank/mode/inference/api/RankApi.scala
@@ -7,15 +7,18 @@ import ai.metarank.mode.inference.RankResponse.{ItemScore, StateValues}
 import ai.metarank.mode.inference.ranking.RankScorer
 import ai.metarank.model.Event.RankingEvent
 import ai.metarank.util.Logging
-import cats.effect.{IO, Ref}
+import cats.effect._
+import cats.implicits._
 import org.http4s.HttpRoutes
 import org.http4s._
 import org.http4s.dsl.io._
 import org.http4s.circe.{jsonEncoderOf, jsonOf}
 import io.circe.syntax._
-import io.findify.featury.model.api.ReadRequest
-import io.findify.featury.values.FeatureStore
+import io.findify.featury.model.Key
+import io.findify.featury.model.api.{ReadRequest, ReadResponse}
 import org.http4s.circe._
+
+import scala.concurrent.duration._
 
 case class RankApi(
     mapping: FeatureMapping,
@@ -35,29 +38,61 @@ case class RankApi(
   }
 
   def rerank(request: RankingEvent, explain: Boolean): IO[RankResponse] = for {
-    keys          <- IO { mapping.keys(request) }
-    storeResource <- storeResourceRef.get
-    store         <- storeResource.storeRef.get
-    state <- store.read(ReadRequest(keys.toList)).handleErrorWith { case ex: Throwable =>
-      for {
-        _                <- IO { logger.warn("error from store, reconnecting", ex) }
-        reconnected      <- storeResource.reconnect()
-        _                <- storeResourceRef.set(reconnected)
-        reconnectedStore <- reconnected.storeRef.get
-        state2           <- reconnectedStore.read(ReadRequest(keys.toList))
-      } yield {
-        state2
-      }
-    }
-    items  <- IO { mapping.map(request, state.features) }
-    query  <- IO { ClickthroughQuery(items, request.id.value, mapping.datasetDescriptor) }
-    scores <- IO { scorer.score(query) }
+    start     <- IO { System.currentTimeMillis() }
+    keys      <- IO { mapping.keys(request) }
+    state     <- readState(keys.toList)
+    stateTook <- IO { System.currentTimeMillis() }
+    items     <- IO { mapping.map(request, state.features) }
+    query     <- IO { ClickthroughQuery(items, request.id.value, mapping.datasetDescriptor) }
+    scores    <- IO { scorer.score(query) }
     result <- explain match {
       case true  => IO { items.zip(scores).map(x => ItemScore(x._1.id, x._2, x._1.values)) }
       case false => IO { items.zip(scores).map(x => ItemScore(x._1.id, x._2, Nil)) }
     }
+    _ <- IO {
+      logger.info(
+        s"processing time: state loading ${stateTook - start}ms, total ${System.currentTimeMillis() - start}ms"
+      )
+    }
+
   } yield {
     RankResponse(state = StateValues(state.features), items = result.sortBy(-_.score))
+  }
+
+  val BATCH_SIZE        = 512
+  val MAX_RECONNECTS    = 3
+  val RECONNECT_TIMEOUT = 50.millis
+
+  def readState(keys: List[Key], attempt: Int = 0): IO[ReadResponse] = for {
+    storeResource <- storeResourceRef.get
+    store         <- storeResource.storeRef.get
+    batches = keys.grouped(BATCH_SIZE).toList
+    state <- batches
+      .map(batch => store.read(ReadRequest(batch)))
+      .sequence
+      .map(x => ReadResponse(x.flatMap(_.features)))
+      .handleErrorWith {
+        case ex: Throwable if attempt >= MAX_RECONNECTS =>
+          logger.error(s"$MAX_RECONNECTS attempts to read ${keys.size} keys failed", ex)
+          IO.raiseError(StateReadError(s"cannot read data from state"))
+        case ex: Throwable =>
+          for {
+            _ <- IO { logger.warn(s"error from store, reconnecting (attempt $attempt of $MAX_RECONNECTS)", ex) }
+            reconnected <- storeResource.reconnect()
+            _           <- storeResourceRef.set(reconnected)
+            _           <- IO.sleep(RECONNECT_TIMEOUT * attempt)
+            state2      <- readState(keys, attempt + 1)
+          } yield {
+            state2
+          }
+      }
+    _ <- IO {
+      logger.info(
+        s"request for ${keys.size} keys split to ${batches.size} batches, loaded ${state.features.size} values"
+      )
+    }
+  } yield {
+    state
   }
 
 }
@@ -67,5 +102,6 @@ object RankApi {
   implicit val requestDecoder: EntityDecoder[IO, RankingEvent]   = jsonOf
   implicit val itemScoreEncoder: EntityEncoder[IO, RankResponse] = jsonEncoderOf
 
-  object ExplainParamDecoder extends OptionalQueryParamDecoderMatcher[Boolean]("explain")
+  object ExplainParamDecoder             extends OptionalQueryParamDecoderMatcher[Boolean]("explain")
+  case class StateReadError(msg: String) extends Exception(msg)
 }
--- src/test/scala/ai/metarank/api/RankApiTest.scala
+++ src/test/scala/ai/metarank/api/RankApiTest.scala
@@ -1,8 +1,9 @@
 package ai.metarank.api
 
-import ai.metarank.api.RankApiTest.BrokenStore
+import ai.metarank.api.RankApiTest.{BrokenStore, CountingStore}
 import ai.metarank.mode.inference.FeatureStoreResource
 import ai.metarank.mode.inference.api.RankApi
+import ai.metarank.mode.inference.api.RankApi.StateReadError
 import ai.metarank.util.{RandomFeatureStore, RandomScorer, TestFeatureMapping, TestRankingEvent}
 import cats.effect.unsafe.implicits.global
 import cats.effect.{IO, Ref}
@@ -15,8 +16,23 @@ import org.scalatest.matchers.should.Matchers
 import scala.util.Try
 
 class RankApiTest extends AnyFlatSpec with Matchers {
+  val mapping = TestFeatureMapping()
+  it should "unfold long requests" in {
+    val counter = new CountingStore()
+    val store = Ref.of[IO, FeatureStoreResource](
+      FeatureStoreResource(
+        storeRef = Ref.of[IO, FeatureStore](counter).unsafeRunSync(),
+        makeStore = () => counter
+      )
+    )
+    val api       = RankApi(mapping, store.unsafeRunSync(), RandomScorer())
+    val request   = TestRankingEvent((0 until 1000).map(i => s"p$i").toList)
+    val response1 = api.rerank(request, false).unsafeRunSync()
+    response1.items.size shouldBe 1000
+    counter.reads shouldBe 12
+    counter.featureReads shouldBe 6001
+  }
   it should "handle protocol errors" in {
-    val mapping = TestFeatureMapping()
     val store = Ref.of[IO, FeatureStoreResource](
       FeatureStoreResource(
         storeRef = Ref.of[IO, FeatureStore](new BrokenStore()).unsafeRunSync(),
@@ -24,15 +40,43 @@ class RankApiTest extends AnyFlatSpec with Matchers {
       )
     )
     val api       = RankApi(mapping, store.unsafeRunSync(), RandomScorer())
+    val response1 = api.rerank(TestRankingEvent(List("p1", "p2", "p3")), false).unsafeRunSync()
+    response1.items.size shouldBe 3
+  }
+
+  it should "fail request on broken state backend" in {
+    val broken = new BrokenStore()
+    val store = Ref.of[IO, FeatureStoreResource](
+      FeatureStoreResource(
+        storeRef = Ref.of[IO, FeatureStore](broken).unsafeRunSync(),
+        makeStore = () => broken
+      )
+    )
+    val api       = RankApi(mapping, store.unsafeRunSync(), RandomScorer())
     val response1 = Try(api.rerank(TestRankingEvent(List("p1", "p2", "p3")), false).unsafeRunSync())
-    response1.isFailure shouldBe false
+    response1.isFailure shouldBe true
   }
 }
 
 object RankApiTest {
   class BrokenStore extends FeatureStore {
-    override def read(request: ReadRequest): IO[ReadResponse] = IO.raiseError(new IllegalArgumentException("oops"))
-    override def write(batch: List[FeatureValue]): IO[Unit]   = IO.raiseError(new IllegalArgumentException("oops"))
-    override def close(): IO[Unit]                            = IO.raiseError(new IllegalArgumentException("oops"))
+    override def read(request: ReadRequest): IO[ReadResponse] = IO.raiseError(StateReadError("oops"))
+    override def write(batch: List[FeatureValue]): IO[Unit]   = IO.raiseError(StateReadError("oops"))
+    override def close(): IO[Unit]                            = IO.raiseError(StateReadError("oops"))
+  }
+
+  class CountingStore extends FeatureStore {
+    var reads        = 0
+    var featureReads = 0
+    var writes       = 0
+    override def read(request: ReadRequest): IO[ReadResponse] = IO {
+      reads += 1
+      featureReads += request.keys.size
+      ReadResponse(Nil)
+    }
+    override def write(batch: List[FeatureValue]): IO[Unit] = IO {
+      writes += 1
+    }
+    override def close(): IO[Unit] = IO.unit
   }
 }

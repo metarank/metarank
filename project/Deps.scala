b585aeeb4ce8fae02e1c02a19c513b22bde24230
Grebennikov Roman <grv@dfdx.me>
2022-04-14 10:48:23 +0200

    make bootstrap savepoint faster

--- project/Deps.scala
+++ project/Deps.scala
@@ -8,7 +8,7 @@ object Deps {
   lazy val circeYamlVersion = "0.14.1"
   lazy val fs2Version       = "3.2.2"
   lazy val flinkVersion     = "1.14.2"
-  lazy val featuryVersion   = "0.3.0-M8-SNAPSHOT"
+  lazy val featuryVersion   = "0.3.0-M9-SNAPSHOT"
   lazy val luceneVersion    = "9.1.0"
 
   val httpsDeps = Seq(

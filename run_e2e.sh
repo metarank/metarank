2fdd12760b8b8a60076cc665165426b22c8456b2
Roman Grebennikov <grv@dfdx.me>
2022-08-17 17:25:17 +0000

    add standalone mode with e2e test (#510)

--- run_e2e.sh
+++ run_e2e.sh
@@ -4,5 +4,19 @@ set -euxo pipefail
 
 JAR=$1
 
-java -jar $JAR bootstrap src/test/resources/ranklens/config.yml
+java -jar $JAR standalone\
+  --config src/test/resources/ranklens/config.yml\
+  --data src/test/resources/ranklens/events/ & echo $! > /tmp/inference.pid
 
+PID=$(cat /tmp/inference.pid)
+
+echo "Waiting for http server with pid=$PID to come online..."
+
+while ! nc -z localhost 8080; do
+  sleep 5
+  echo "Trying to connect to :8080"
+done
+
+curl -v http://localhost:8080/health
+
+kill -TERM $PID

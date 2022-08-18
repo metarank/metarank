7d51b73b629b365368f0e28ae73aad2e49d3aef0
Roman Grebennikov <grv@dfdx.me>
2022-08-18 13:37:40 +0000

    remove unused dependencies (#519)
    * remove unused dependencies
    
    * docker: make fewer image layers

--- src/test/scala/ai/metarank/fstore/redis/RedisFeatureTest.scala
+++ src/test/scala/ai/metarank/fstore/redis/RedisFeatureTest.scala
@@ -1,17 +1,9 @@
 package ai.metarank.fstore.redis
 
 import ai.metarank.fstore.FeatureSuite
-import ai.metarank.fstore.redis.client.RedisPipeline.RedisOp
-import ai.metarank.fstore.redis.client.{RedisPipeline, RedisClient}
 import ai.metarank.model.{Feature, FeatureValue, Write}
-import cats.effect.IO
-import cats.effect.std.Queue
 import cats.effect.unsafe.implicits.global
-import com.github.microwww.redis.RedisServer
 import org.scalatest.BeforeAndAfterAll
-import redis.clients.jedis.Jedis
-
-import scala.util.Random
 
 trait RedisFeatureTest[W <: Write, F <: Feature[W, _ <: FeatureValue]] extends BeforeAndAfterAll with RedisTest {
   this: FeatureSuite[W] =>

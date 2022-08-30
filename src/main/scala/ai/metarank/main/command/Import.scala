dbee7a8176dd5fca800fc352cf34f4d45bae30bc
Grebennikov Roman <grv@dfdx.me>
2022-08-30 14:05:56 +0200

    do not enforce sorting, auto-sort on import

--- src/main/scala/ai/metarank/main/command/Import.scala
+++ src/main/scala/ai/metarank/main/command/Import.scala
@@ -4,12 +4,14 @@ import ai.metarank.FeatureMapping
 import ai.metarank.config.Config
 import ai.metarank.config.InputConfig.FileInputConfig
 import ai.metarank.flow.MetarankFlow.ProcessResult
-import ai.metarank.flow.{ClickthroughImpressionFlow, FeatureValueFlow, FeatureValueSink, MetarankFlow, OrderCheckFlow}
+import ai.metarank.flow.MetarankFlow
 import ai.metarank.fstore.Persistence
 import ai.metarank.main.CliArgs.ImportArgs
 import ai.metarank.model.Event
 import ai.metarank.source.FileEventSource
 import ai.metarank.util.Logging
+import ai.metarank.validate.EventValidation.ValidationError
+import ai.metarank.validate.checks.EventOrderValidation.EventOrderError
 import cats.effect.IO
 import cats.effect.kernel.Resource
 
@@ -29,9 +31,25 @@ object Import extends Logging {
   }
 
   def slurp(store: Persistence, mapping: FeatureMapping, args: ImportArgs, conf: Config): IO[ProcessResult] = {
-    val stream   = FileEventSource(FileInputConfig(args.data.toString, args.offset, args.format)).stream
-    val validate = IO.whenA(args.validation)(Validate.validate(conf, stream).void)
-    validate *> slurp(stream, store, mapping)
+    val stream = FileEventSource(FileInputConfig(args.data.toString, args.offset, args.format)).stream
+    for {
+      errors       <- validated(conf, stream, args.validation)
+      sortedStream <- sorted(stream, errors)
+      result       <- slurp(sortedStream, store, mapping)
+    } yield {
+      result
+    }
+  }
+
+  def validated(conf: Config, stream: fs2.Stream[IO, Event], flag: Boolean): IO[List[ValidationError]] =
+    if (flag) Validate.validate(conf, stream) else IO.pure(Nil)
+
+  def sorted(stream: fs2.Stream[IO, Event], errors: List[ValidationError]): IO[fs2.Stream[IO, Event]] = IO {
+    val shouldSort = errors.collectFirst { case EventOrderError(_) => }.isDefined
+    if (shouldSort) {
+      logger.warn("Dataset seems not to be sorted by timestamp, doing in-memory sort")
+      fs2.Stream.evalSeq(stream.compile.toList.map(_.sortBy(_.timestamp.ts))).chunkN(1024).unchunks
+    } else stream
   }
 
   def slurp(source: fs2.Stream[IO, Event], store: Persistence, mapping: FeatureMapping): IO[ProcessResult] = {

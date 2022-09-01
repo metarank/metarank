9f2c45c73419ad79d5fe303988a403a8b6f666c2
Grebennikov Roman <grv@dfdx.me>
2022-09-01 15:00:43 +0200

    fix crash on macos MAC address reading

--- src/main/scala/ai/metarank/model/AnalyticsPayload.scala
+++ src/main/scala/ai/metarank/model/AnalyticsPayload.scala
@@ -103,10 +103,10 @@ object AnalyticsPayload {
       .networkInterfaces()
       .collect(Collectors.toList[NetworkInterface])
       .asScala
-      .filter(iface => iface.isUp && !iface.isLoopback)
+      .filter(iface => iface.isUp && !iface.isLoopback && !iface.isVirtual)
+      .flatMap(iface => Option(iface.getHardwareAddress))
 
     interfaces.headOption
-      .map(_.getHardwareAddress)
       .map(addr => Hashing.sha256().hashBytes(addr).toString)
   }
 }

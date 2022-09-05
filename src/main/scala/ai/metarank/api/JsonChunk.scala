f9d7b71e4f18f221f719b0e1f133212bd58a8580
Roman Grebennikov <grv@dfdx.me>
2022-09-05 09:35:56 +0000

    bump http4s to 1.0.0-M35 (#610)

--- src/main/scala/ai/metarank/api/JsonChunk.scala
+++ src/main/scala/ai/metarank/api/JsonChunk.scala
@@ -3,6 +3,7 @@ package ai.metarank.api
 import fs2.Chunk
 import io.circe.{Encoder, Printer}
 import io.circe.syntax._
+import scodec.bits.ByteVector
 
 object JsonChunk {
   val jsonFormat = Printer(
@@ -11,7 +12,7 @@ object JsonChunk {
     indent = "  "
   )
 
-  def apply[T: Encoder](value: T): Chunk[Byte] = {
-    Chunk.array(jsonFormat.print(value.asJson).getBytes())
+  def apply[T: Encoder](value: T): ByteVector = {
+    ByteVector.apply(jsonFormat.print(value.asJson).getBytes())
   }
 }

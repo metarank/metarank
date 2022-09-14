e729a20867eadcc0748700743d8ba64e068ad99c
Grebennikov Roman <grv@dfdx.me>
2022-09-14 12:04:21 +0200

    fix crash on test dataset, more logging

--- src/main/scala/ai/metarank/main/Sort.scala
+++ src/main/scala/ai/metarank/main/Sort.scala
@@ -14,6 +14,7 @@ import org.apache.commons.io.FileUtils
 
 import java.io.{BufferedInputStream, BufferedOutputStream, FileInputStream, FileOutputStream, InputStream, OutputStream}
 import java.nio.file.{Files, Path}
+import java.util.Comparator
 import scala.jdk.CollectionConverters._
 
 object Sort extends Logging {
@@ -21,9 +22,13 @@ object Sort extends Logging {
     override def compareTo(t: SortableEvent): Int = java.lang.Long.compare(ts.ts, t.ts.ts)
   }
 
+  object EventReaderFactory extends DataReaderFactory[SortableEvent] {
+    override def constructReader(in: InputStream): DataReader[SortableEvent] = new EventReader(in)
+  }
+
   class EventReader(in: InputStream) extends DataReader[SortableEvent] {
     val nested   = new RawTextLineReader(in)
-    var lastSize = 1024
+    var lastSize = 0
     var count    = 0L
 
     override def readNext(): SortableEvent = {
@@ -33,9 +38,9 @@ object Sort extends Logging {
           logger.info(s"EOF reached, read $count unsorted events")
           null
         case Some(lineBytes) =>
-          lastSize = next.length
+          lastSize += next.length
           count += 1
-          if (count % 1234 == 0) {
+          if (count % 12345 == 0) {
             logger.info(s"read $count unsorted events")
           }
           val line = new String(lineBytes)
@@ -48,8 +53,20 @@ object Sort extends Logging {
       }
     }
 
-    override def estimateSizeInBytes(item: SortableEvent): Int = lastSize
+    override def estimateSizeInBytes(item: SortableEvent): Int = math.round(lastSize.toFloat / count.toFloat)
     override def close(): Unit                                 = nested.close()
+  }
+  object EventWriterFactory extends DataWriterFactory[SortableEvent] {
+    override def constructWriter(out: OutputStream): DataWriter[SortableEvent] = new EventWriter(out)
+  }
+  class EventWriter(out: OutputStream) extends DataWriter[SortableEvent] {
+    val nested                                         = new RawTextLineWriter(out)
+    override def writeEntry(item: SortableEvent): Unit = nested.writeEntry(item.json.getBytes())
+    override def close(): Unit                         = nested.close()
+  }
+
+  object EventTimestampComparator extends Comparator[SortableEvent] {
+    override def compare(t: SortableEvent, t1: SortableEvent): Int = t.compareTo(t1)
   }
 
   def run(args: SortArgs): IO[Unit] = {
@@ -87,24 +104,27 @@ object Sort extends Logging {
 
     }
   }
+  val MAX_MEM = 128 * 1024 * 1024
 
   def sortFile(in: Path, out: Path): IO[Unit] = IO {
-    val sorter         = new Sorter[SortableEvent](new SortConfig())
-    val source         = new BufferedInputStream(new FileInputStream(in.toFile), 1024 * 1024)
-    val dest           = new BufferedOutputStream(new FileOutputStream(out.toFile), 1024 * 1024)
+    val conf = new SortConfig().withMaxMemoryUsage(MAX_MEM)
+    logger.info(s"using ${FileUtils.byteCountToDisplaySize(MAX_MEM)} RAM for on-disk merge sort")
+    val sorter = new Sorter[SortableEvent](conf, EventReaderFactory, EventWriterFactory, EventTimestampComparator)
+    val source = new BufferedInputStream(new FileInputStream(in.toFile), 1024 * 1024)
+    val dest   = new BufferedOutputStream(new FileOutputStream(out.toFile), 1024 * 1024)
     val sortedIterator = sorter.sort(new EventReader(source))
-    logger.info(s"on-disk merge pre-sorting done")
+    logger.info(s"on-disk merge pre-sorting done, ${sorter.getNumberOfPreSortFiles} shards")
     var count     = 0
     var byteCount = 0L
     sortedIterator.asScala.foreach(e => {
       count += 1
       val bytes = (e.json + "\n").getBytes()
       byteCount += bytes.length
-      if (count % 1234 == 0) logger.info(s"wrote $count sorted events, ${FileUtils.byteCountToDisplaySize(byteCount)}")
+      if (count % 12345 == 0) logger.info(s"wrote $count sorted events, ${FileUtils.byteCountToDisplaySize(byteCount)}")
       dest.write(bytes)
     })
     source.close()
     dest.close()
-    logger.info(s"written sorted file: ${out.toString}, ${FileUtils.byteCountToDisplaySize(byteCount)}")
+    logger.info(s"written sorted file: ${out.toString}, ${FileUtils.byteCountToDisplaySize(byteCount)}, $count events")
   }
 }

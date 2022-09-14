d7ccc9ce3bd6be88106a8dcbff704754fd23ffc8
Grebennikov Roman <grv@dfdx.me>
2022-09-14 11:48:24 +0200

    add support for dir sorting, with extra logging

--- src/test/scala/ai/metarank/main/SortTest.scala
+++ src/test/scala/ai/metarank/main/SortTest.scala
@@ -2,6 +2,7 @@ package ai.metarank.main
 
 import ai.metarank.config.InputConfig.FileInputConfig
 import ai.metarank.main.CliArgs.SortArgs
+import ai.metarank.model.Event
 import ai.metarank.source.FileEventSource
 import ai.metarank.util.SyntheticRanklensDataset
 import cats.effect.unsafe.implicits.global
@@ -9,23 +10,47 @@ import org.scalatest.flatspec.AnyFlatSpec
 import org.scalatest.matchers.should.Matchers
 
 import java.io.{BufferedOutputStream, FileOutputStream}
-import java.nio.file.Files
+import java.nio.file.{Files, Path}
 import scala.util.Random
 import io.circe.syntax._
 
 class SortTest extends AnyFlatSpec with Matchers {
+  lazy val events = Random.shuffle(SyntheticRanklensDataset(items = 100, users = 100))
+
   it should "sort single file" in {
-    val events = Random.shuffle(SyntheticRanklensDataset(items = 100, users = 100))
-    val file   = Files.createTempFile("events_in_", ".json")
-    val out    = Files.createTempFile("events_out_", ".json")
+    val file = Files.createTempFile("events_in_", ".json")
+    file.toFile.deleteOnExit()
+    val out = Files.createTempFile("events_out_", ".json")
+    out.toFile.deleteOnExit()
+    writeBatch(events, file)
+
+    Sort.run(SortArgs(null, file, out)).unsafeRunSync()
+    val sorted = FileEventSource(FileInputConfig(out.toString)).stream.compile.toList.unsafeRunSync()
+    sorted shouldBe events.sortBy(_.timestamp.ts)
+  }
+
+  it should "read directory of unsorted files" in {
+    val dir = Files.createTempDirectory("events_in")
+    val out = Files.createTempFile("events_out_", ".json")
+    out.toFile.deleteOnExit()
+    for {
+      (batch, index) <- events.grouped(1000).zipWithIndex
+    } {
+      val file = Files.createTempFile(dir, s"events_in_${index}_", ".json")
+      file.toFile.deleteOnExit()
+      writeBatch(batch, file)
+    }
+    Sort.run(SortArgs(null, dir, out)).unsafeRunSync()
+    val sorted = FileEventSource(FileInputConfig(out.toString)).stream.compile.toList.unsafeRunSync()
+    sorted shouldBe events.sortBy(_.timestamp.ts)
+  }
+
+  def writeBatch(events: List[Event], file: Path) = {
     val stream = new BufferedOutputStream(new FileOutputStream(file.toFile), 10 * 1024)
     events.foreach(event => {
       stream.write(event.asJson.noSpaces.getBytes())
       stream.write('\n'.toInt)
     })
     stream.close()
-    Sort.run(SortArgs(null, file, out)).unsafeRunSync()
-    val sorted = FileEventSource(FileInputConfig(out.toString)).stream.compile.toList.unsafeRunSync()
-    sorted shouldBe events.sortBy(_.timestamp.ts)
   }
 }

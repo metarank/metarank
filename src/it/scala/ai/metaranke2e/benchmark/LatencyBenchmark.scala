cb8a091306452dfc60c6cbf98ed2af92894a31c6
Grebennikov Roman <grv@dfdx.me>
2022-10-13 12:04:58 +0200

    wip: latency benchmark

---
+++ src/it/scala/ai/metaranke2e/benchmark/LatencyBenchmark.scala
@@ -0,0 +1,7 @@
+package ai.metaranke2e.benchmark
+
+import cats.effect.{ExitCode, IO, IOApp}
+
+object LatencyBenchmark extends IOApp {
+  override def run(args: List[String]): IO[ExitCode] = ???
+}

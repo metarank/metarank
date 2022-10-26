ae488b0f84e2d0b79e30cb0eed7b1e6be4c91eb3
Scala Steward <43047562+scala-steward@users.noreply.github.com>
2022-10-26 12:50:39 +0200

    Update circe-generic-extras to 0.14.3 (#701)

--- project/Deps.scala
+++ project/Deps.scala
@@ -5,7 +5,7 @@ object Deps {
   lazy val log4catsVersion           = "2.4.0"
   lazy val scalatestVersion          = "3.2.14"
   lazy val circeVersion              = "0.14.3"
-  lazy val circeGenericExtrasVersion = "0.14.2"
+  lazy val circeGenericExtrasVersion = "0.14.3"
   lazy val circeYamlVersion          = "0.14.1"
   lazy val fs2Version                = "3.2.2"
   lazy val pulsarVersion             = "2.10.2"

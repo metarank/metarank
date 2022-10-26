64181cbe4e664fa83851fdff354b847c217b3f9a
Grebennikov Roman <grv@dfdx.me>
2022-10-26 13:19:48 +0200

    wait in e2e tests till /health responds 200

--- .github/test_docker.sh
+++ .github/test_docker.sh
@@ -2,6 +2,14 @@
 
 set -euxo pipefail
 
-docker run -d -v `pwd`/src/test/resources/ranklens/:/data metarank/metarank:snapshot serve --config /data/config.yml
+DIR=`pwd`
 
-curl -v http://localhost:8080/health
+CONTAINER_ID=`docker run -d -p 8080:8080 -v $DIR/src/test/resources/ranklens/:/data metarank/metarank:snapshot serve --config /data/config.yml`
+
+echo "Trying to connect to :8080"
+
+timeout 30 sh -c 'until curl -v http://localhost:8080/health; do sleep 1; done'
+
+echo "Stopping container $CONTAINER_ID"
+
+docker kill $CONTAINER_ID
--- run_quickstart.sh
+++ run_quickstart.sh
@@ -12,10 +12,7 @@ PID=$(cat /tmp/inference.pid)
 
 echo "Waiting for http server with pid=$PID to come online..."
 
-while ! nc -z localhost 8080; do
-  sleep 5
-  echo "Trying to connect to :8080"
-done
+timeout 30 sh -c 'until curl -v http://localhost:8080/health; do sleep 1; done'
 
 curl -X POST http://localhost:8080/rank/xgboost -d '{
     "event": "ranking",

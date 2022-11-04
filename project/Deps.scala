80d57fe4b8e37c4e6231983768b6c298353e40cd
Scala Steward <scala_steward@virtuslab.com>
2022-11-04 20:53:23 +0000

    Update circe-yaml to 0.14.2

--- project/Deps.scala
+++ project/Deps.scala
@@ -6,7 +6,7 @@ object Deps {
   lazy val scalatestVersion          = "3.2.14"
   lazy val circeVersion              = "0.14.3"
   lazy val circeGenericExtrasVersion = "0.14.3"
-  lazy val circeYamlVersion          = "0.14.1"
+  lazy val circeYamlVersion          = "0.14.2"
   lazy val fs2Version                = "3.2.2"
   lazy val pulsarVersion             = "2.10.2"
   lazy val luceneVersion             = "9.4.1"

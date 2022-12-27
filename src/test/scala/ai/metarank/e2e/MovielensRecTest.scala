fefc4625fc914958d4f9b02cd077cd247e3fc3b1
Grebennikov Roman <grv@dfdx.me>
2022-12-27 16:17:57 +0100

    movielens reader

---
+++ src/test/scala/ai/metarank/e2e/MovielensRecTest.scala
@@ -0,0 +1,8 @@
+package ai.metarank.e2e
+
+import org.scalatest.flatspec.AnyFlatSpec
+import org.scalatest.matchers.should.Matchers
+
+class MovielensRecTest extends AnyFlatSpec with Matchers {
+
+}

24da36d3c2753765bf4ed37a5060ba5f3a55106f
Roman Grebennikov <grv@dfdx.me>
2023-01-31 15:50:01 +0100

    add sbt -mem 3000 to docker build

--- build_docker.sh
+++ build_docker.sh
@@ -6,8 +6,8 @@ V=$1
 
 docker run --rm --privileged multiarch/qemu-user-static --reset -p yes
 
-PLATFORM=amd64 VERSION=$V sbt dockerBuildAndPush
-PLATFORM=arm64 VERSION=$V sbt dockerBuildAndPush
+PLATFORM=amd64 VERSION=$V sbt -mem 3000 dockerBuildAndPush
+PLATFORM=arm64 VERSION=$V sbt -mem 3000 dockerBuildAndPush
 
 docker manifest create --amend metarank/metarank:$V metarank/metarank:$V-arm64 metarank/metarank:$V-amd64
 docker manifest create --amend metarank/metarank:latest metarank/metarank:$V-arm64 metarank/metarank:$V-amd64

2889f12632fcea78c8a01bb73250bf1201a4891b
Grebennikov Roman <grv@dfdx.me>
2023-02-07 10:04:19 +0100

    fix issue with mfconfig generator

--- src/main/scala/ai/metarank/main/command/autofeature/model/SimilarRecsConfigGenerator.scala
+++ src/main/scala/ai/metarank/main/command/autofeature/model/SimilarRecsConfigGenerator.scala
@@ -2,14 +2,13 @@ package ai.metarank.main.command.autofeature.model
 
 import ai.metarank.main.command.autofeature.model.ModelGenerator.ModelConfigMirror
 import ai.metarank.ml.recommend.TrendingRecommender.{InteractionWeight, TrendingConfig}
+import ai.metarank.ml.recommend.mf.ALSRecImpl.ALSConfig
 
 object SimilarRecsConfigGenerator extends RecsConfigGenerator {
   override def name: String = "similar"
 
   override def makeConfig(ints: List[String]): ModelConfigMirror = ModelConfigMirror(
     name = name,
-    conf = TrendingConfig(
-      weights = ints.map(it => InteractionWeight(it))
-    )
+    conf = ALSConfig(interactions = ints)
   )
 }
--- src/test/scala/ai/metarank/main/autofeature/model/SimilarRecsConfigGeneratorTest.scala
+++ src/test/scala/ai/metarank/main/autofeature/model/SimilarRecsConfigGeneratorTest.scala
@@ -2,9 +2,9 @@ package ai.metarank.main.autofeature.model
 
 import ai.metarank.feature.NumberFeature.NumberFeatureSchema
 import ai.metarank.main.command.autofeature.model.ModelGenerator.ModelConfigMirror
-import ai.metarank.main.command.autofeature.model.TrendingRecsConfigGenerator
 import ai.metarank.main.command.autofeature.{EventCountStat, EventModel, InteractionStat}
-import ai.metarank.ml.recommend.TrendingRecommender.{InteractionWeight, TrendingConfig}
+import ai.metarank.main.command.autofeature.model.{SimilarRecsConfigGenerator, TrendingRecsConfigGenerator}
+import ai.metarank.ml.recommend.mf.ALSRecImpl.ALSConfig
 import ai.metarank.model.FieldName
 import ai.metarank.model.FieldName.EventType.Item
 import ai.metarank.model.Key.FeatureName
@@ -12,18 +12,18 @@ import ai.metarank.model.ScopeType.ItemScopeType
 import org.scalatest.flatspec.AnyFlatSpec
 import org.scalatest.matchers.should.Matchers
 
-class TrendingRecsConfigGeneratorTest extends AnyFlatSpec with Matchers {
-  it should "generate trending recs when enough clicks" in {
+class SimilarRecsConfigGeneratorTest extends AnyFlatSpec with Matchers {
+  it should "generate similar recs when enough clicks" in {
     val em = EventModel(eventCount = EventCountStat(ints = 100), interactions = InteractionStat(Map("click" -> 100)))
     val features = List(NumberFeatureSchema(FeatureName("foo"), FieldName(Item, "foo"), ItemScopeType))
-    val result   = TrendingRecsConfigGenerator.maybeGenerate(em, features)
-    result shouldBe Some(ModelConfigMirror("trending", TrendingConfig(List(InteractionWeight("click", 1.0)))))
+    val result   = SimilarRecsConfigGenerator.maybeGenerate(em, features)
+    result shouldBe Some(ModelConfigMirror("similar", ALSConfig(List("click"))))
   }
 
   it should "fail if there's not enough clicks" in {
     val em       = EventModel(eventCount = EventCountStat(ints = 1), interactions = InteractionStat(Map("click" -> 1)))
     val features = List(NumberFeatureSchema(FeatureName("foo"), FieldName(Item, "foo"), ItemScopeType))
-    val result   = TrendingRecsConfigGenerator.maybeGenerate(em, features)
+    val result   = SimilarRecsConfigGenerator.maybeGenerate(em, features)
     result shouldBe None
   }
 }

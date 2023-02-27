7127f9c9a1e3d4d2337b0ea309eaf26df4602841
Roman Grebennikov <grv@dfdx.me>
2023-02-27 18:17:40 +0100

    check for too large dataset size (#925)

--- src/test/scala/ai/metarank/ml/rank/LambdaMARTRankerTest.scala
+++ src/test/scala/ai/metarank/ml/rank/LambdaMARTRankerTest.scala
@@ -10,7 +10,7 @@ import ai.metarank.util.{TestClickthroughValues, TestQueryRequest}
 import cats.data.NonEmptyList
 import cats.effect.unsafe.implicits.global
 import io.github.metarank.ltrlib.model.DatasetDescriptor
-import io.github.metarank.ltrlib.model.Feature.SingularFeature
+import io.github.metarank.ltrlib.model.Feature.{SingularFeature, VectorFeature}
 
 import scala.util.{Failure, Try}
 
@@ -32,5 +32,19 @@ class LambdaMARTRankerTest extends PredictorSuite[LambdaMARTConfig, QueryRequest
     val err = Try(predictor.fit(fs2.Stream(cts.map(_.copy(values = Nil)): _*)).unsafeRunSync())
     err should matchPattern { case Failure(ex: EmptyDatasetException) => // yep
     }
+  }
+
+  it should "fail when dataset is too large" in {
+    val result = Try(
+      LambdaMARTRanker
+        .checkDatasetSize(
+          itemCount = 3000000,
+          dim = 1000,
+          groupsCount = 30000,
+          List(SingularFeature("foo"), VectorFeature("bar", 999))
+        )
+        .unsafeRunSync()
+    )
+    result shouldBe a[Failure[_]]
   }
 }

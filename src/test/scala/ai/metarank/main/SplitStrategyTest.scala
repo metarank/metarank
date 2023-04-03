2836c943fa6a28fa8025e0a8ea01c538c99e1f86
Roman Grebennikov <grv@dfdx.me>
2023-04-04 00:48:32 +0200

    field split strategy (#993)

--- src/test/scala/ai/metarank/main/SplitStrategyTest.scala
+++ src/test/scala/ai/metarank/main/SplitStrategyTest.scala
@@ -1,7 +1,8 @@
 package ai.metarank.main
 
 import ai.metarank.main.command.train.SplitStrategy
-import ai.metarank.main.command.train.SplitStrategy.{RandomSplit, TimeSplit}
+import ai.metarank.main.command.train.SplitStrategy.{FieldStrategy, RandomSplit, TimeSplit}
+import ai.metarank.model.Field.StringField
 import ai.metarank.model.{QueryMetadata, Timestamp}
 import cats.effect.unsafe.implicits.global
 import io.github.metarank.ltrlib.model.{DatasetDescriptor, LabeledItem, Query}
@@ -13,11 +14,12 @@ class SplitStrategyTest extends AnyFlatSpec with Matchers {
   it should "parse inputs" in {
     SplitStrategy.parse("random=10%") shouldBe Right(RandomSplit(10))
     SplitStrategy.parse("random") shouldBe Right(RandomSplit(80))
+    SplitStrategy.parse("field=split:train:test") shouldBe Right(FieldStrategy("split", "train", "test"))
   }
 
   val desc  = DatasetDescriptor(List(SingularFeature("foo")))
   val now   = Timestamp.now
-  val query = QueryMetadata(Query(desc, List(LabeledItem(1.0, 1, Array(1.0)))), now, None)
+  val query = QueryMetadata(Query(desc, List(LabeledItem(1.0, 1, Array(1.0)))), now, None, Nil)
 
   "time-split" should "handle unbalanced small inputs, size=2" in {
     val split = TimeSplit(80).split(desc, List(query, query)).unsafeRunSync()
@@ -29,5 +31,19 @@ class SplitStrategyTest extends AnyFlatSpec with Matchers {
     val split = TimeSplit(80).split(desc, List(query, query, query)).unsafeRunSync()
     split.test.groups.size shouldBe 1
     split.train.groups.size shouldBe 2
+  }
+
+  "field split" should "split by field value" in {
+    val result = FieldStrategy("split", "train", "test")
+      .split(
+        desc,
+        List(
+          query.copy(fields = List(StringField("split", "train"))),
+          query.copy(fields = List(StringField("split", "test")))
+        )
+      )
+      .unsafeRunSync()
+    result.test.groups.size shouldBe 1
+    result.train.groups.size shouldBe 1
   }
 }

0871325cbd4dfa9c7f674e2b49f5e343a332825f
Scala Steward <scala_steward@virtuslab.com>
2023-07-30 17:07:14 +0000

    Update kinesis to 2.20.115

--- project/Deps.scala
+++ project/Deps.scala
@@ -10,7 +10,7 @@ object Deps {
   lazy val fs2Version                = "3.2.2"
   lazy val pulsarVersion             = "2.11.1"
   lazy val luceneVersion             = "9.6.0"
-  lazy val awsVersion                = "2.20.48"
+  lazy val awsVersion                = "2.20.115"
   lazy val prometheusVersion         = "0.16.0"
 
   val httpsDeps = Seq(

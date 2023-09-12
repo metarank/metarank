56b62d793eeb9b414831f060c4fc31738f6df1a4
Scala Steward <scala_steward@virtuslab.com>
2023-09-12 21:04:34 +0000

    Update scalatest to 3.2.17

--- project/Deps.scala
+++ project/Deps.scala
@@ -3,7 +3,7 @@ import sbt._
 object Deps {
   lazy val http4sVersion             = "1.0.0-M38"
   lazy val log4catsVersion           = "2.4.0"
-  lazy val scalatestVersion          = "3.2.16"
+  lazy val scalatestVersion          = "3.2.17"
   lazy val circeVersion              = "0.14.6"
   lazy val circeGenericExtrasVersion = "0.14.3"
   lazy val circeYamlVersion          = "0.14.2"

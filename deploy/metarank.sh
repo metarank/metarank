84811a77316fd49cb8d00494465b132f03f8a6a1
Grebennikov Roman <grv@dfdx.me>
2023-11-28 20:24:21 +0100

    add support for custom loggers

--- deploy/metarank.sh
+++ deploy/metarank.sh
@@ -3,5 +3,5 @@
 set -euxo pipefail
 OPTS=${JAVA_OPTS:-"-Xmx1g -verbose:gc"}
 
-exec /usr/bin/java $OPTS -jar /app/metarank.jar "$@"
+exec /usr/bin/java $OPTS -cp "/app/*" ai.metarank.main.Main "$@"
 

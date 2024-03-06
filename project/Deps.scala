e2eca23c95c353f137b5d5c1cd436c242cbdc970
Scala Steward <scala_steward@virtuslab.com>
2024-03-06 18:36:11 +0000

    Update lucene-analysis-common, ... to 9.10.0

--- project/Deps.scala
+++ project/Deps.scala
@@ -9,7 +9,7 @@ object Deps {
   lazy val circeYamlVersion          = "0.15.1"
   lazy val fs2Version                = "3.2.2"
   lazy val pulsarVersion             = "3.2.0"
-  lazy val luceneVersion             = "9.9.2"
+  lazy val luceneVersion             = "9.10.0"
   lazy val awsVersion                = "2.23.21"
   lazy val prometheusVersion         = "0.16.0"
 

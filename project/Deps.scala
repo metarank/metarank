5a3e6487c5e1f8805df5e160ac23c7608a6e9994
Scala Steward <scala_steward@virtuslab.com>
2024-05-17 16:45:33 +0000

    Update pulsar-client, pulsar-client-admin to 3.2.3

--- project/Deps.scala
+++ project/Deps.scala
@@ -8,11 +8,11 @@ object Deps {
   lazy val circeGenericExtrasVersion = "0.14.3"
   lazy val circeYamlVersion          = "0.15.1"
   lazy val fs2Version                = "3.10.2"
-  lazy val pulsarVersion             = "3.2.2"
+  lazy val pulsarVersion             = "3.2.3"
   lazy val luceneVersion             = "9.10.0"
   lazy val awsVersion                = "2.25.15"
   lazy val prometheusVersion         = "0.16.0"
-  lazy val djlVersion = "0.27.0"
+  lazy val djlVersion                = "0.27.0"
 
   val httpsDeps = Seq(
     "org.http4s" %% "http4s-dsl"          % http4sVersion,

400f4a14edd479eed2ccbdae21aa8bd14c304f88
Scala Steward <scala_steward@virtuslab.com>
2024-11-01 15:39:59 +0000

    Update http4s-circe, http4s-dsl, ... to 1.0.0-M43

--- project/Deps.scala
+++ project/Deps.scala
@@ -1,7 +1,7 @@
 import sbt._
 
 object Deps {
-  lazy val http4sVersion             = "1.0.0-M41"
+  lazy val http4sVersion             = "1.0.0-M43"
   lazy val log4catsVersion           = "2.4.0"
   lazy val scalatestVersion          = "3.2.19"
   lazy val circeVersion              = "0.14.9"

b7595d529e577b8e9421d5c26fd0f1dc3eda5dce
Scala Steward <scala_steward@virtuslab.com>
2025-04-22 16:48:21 +0000

    Update circe-core, circe-generic, ... to 0.14.13

--- project/Deps.scala
+++ project/Deps.scala
@@ -4,7 +4,7 @@ object Deps {
   lazy val http4sVersion             = "1.0.0-M41"
   lazy val log4catsVersion           = "2.4.0"
   lazy val scalatestVersion          = "3.2.19"
-  lazy val circeVersion              = "0.14.9"
+  lazy val circeVersion              = "0.14.13"
   lazy val circeGenericExtrasVersion = "0.14.3"
   lazy val circeYamlVersion          = "0.15.3"
   lazy val fs2Version                = "3.10.2"

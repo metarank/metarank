d624ea5478491cd88358842dcd1e8c53e924ea37
Scala Steward <43047562+scala-steward@users.noreply.github.com>
2025-06-24 22:24:16 +0200

    Update kinesis, s3 to 2.31.69 (#1497)

--- project/Deps.scala
+++ project/Deps.scala
@@ -10,7 +10,7 @@ object Deps {
   lazy val fs2Version                = "3.12.0"
   lazy val pulsarVersion             = "3.3.7"
   lazy val luceneVersion             = "9.11.1"
-  lazy val awsVersion                = "2.31.68"
+  lazy val awsVersion                = "2.31.69"
   lazy val prometheusVersion         = "0.16.0"
   lazy val djlVersion                = "0.28.0"
 

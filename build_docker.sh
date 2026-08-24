ca5f65415bcf84c0497ee81cb6144fa7627040a1
Grebennikov Roman <grv@dfdx.me>
2026-08-24 18:04:59 +0200

    add multiarch/qemu-user-static to build_docker

--- build_docker.sh
+++ build_docker.sh
@@ -2,6 +2,8 @@
 
 set -euxo pipefail
 
+docker run --rm --privileged multiarch/qemu-user-static --reset -p yes
+
 # buildx builds and pushes amd64+arm64 images with a multi-arch manifest,
 # tagged from the version in build.sbt: :<version>, :latest and :snapshot
 sbt -mem 5000 Docker/publish

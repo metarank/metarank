cef739acfceaa1d641fc2adc98673a6047a4635a
Scala Steward <scala_steward@virtuslab.com>
2026-09-06 20:19:03 +0000

    Update fs2-core, fs2-io to 3.14.0

--- project/Deps.scala
+++ project/Deps.scala
@@ -1,15 +1,15 @@
 import sbt.*
 
 object Deps {
-  lazy val http4sVersion     = "1.0.0-M47"
-  lazy val log4catsVersion   = "2.8.0"
-  lazy val scalatestVersion  = "3.2.20"
-  lazy val circeVersion      = "0.14.16"
-  lazy val circeYamlVersion  = "0.16.1"
-  lazy val fs2Version        = "3.13.0"
-  lazy val luceneVersion     = "10.5.1"
-  lazy val pulsarVersion     = "4.2.4"
-  lazy val awsVersion        = "2.54.3"
+  lazy val http4sVersion    = "1.0.0-M47"
+  lazy val log4catsVersion  = "2.8.0"
+  lazy val scalatestVersion = "3.2.20"
+  lazy val circeVersion     = "0.14.16"
+  lazy val circeYamlVersion = "0.16.1"
+  lazy val fs2Version       = "3.14.0"
+  lazy val luceneVersion    = "10.5.1"
+  lazy val pulsarVersion    = "4.2.4"
+  lazy val awsVersion       = "2.54.3"
 
   lazy val prometheusVersion = "0.16.0"
   lazy val djlVersion        = "0.36.0"

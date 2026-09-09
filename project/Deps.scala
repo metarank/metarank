e3b8d44b302c98bd7cb2e06bbb6ad06bb3a10293
Scala Steward <43047562+scala-steward@users.noreply.github.com>
2026-09-09 09:56:09 +0200

    Update http4s-circe, http4s-dsl, ... to 1.0.0-M48 (#1700)
    Co-authored-by: Roman Grebennikov <grv@dfdx.me>

--- project/Deps.scala
+++ project/Deps.scala
@@ -1,7 +1,7 @@
 import sbt.*
 
 object Deps {
-  lazy val http4sVersion     = "1.0.0-M47"
+  lazy val http4sVersion     = "1.0.0-M48"
   lazy val log4catsVersion   = "2.8.0"
   lazy val scalatestVersion  = "3.2.20"
   lazy val circeVersion      = "0.14.16"
